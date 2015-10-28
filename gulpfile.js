04c870a9124ef04e7844388ef996d0ee92cc6d3a
Chris DeMars <cdemars86@gmail.com>
2015-10-28 14:00:11 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// Gulp packages
+
+var gulp  = require('gulp');
+    gutil = require('gulp-util');
+
+// Create a default task and log a message.
+
+gulp.task('default', function() {
+    return gutil.log('Gulp is running!')
+ });
+
