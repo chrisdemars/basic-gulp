786b56fe55d86aa420759e56af015a8ba5c88c44
TJ Miller <tj@sixlabs.io>
2016-02-26 18:50:46 -0500

    patched issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,49 @@
  // Gulp packages
 
-var gulp     = require('gulp');
-    concat   = require('gulp-concat');
-    imagemin = require('gulp-imagemin');
-    jshint   = require('gulp-jshint');
-    strip    = require('gulp-strip-debug');
-    uglify   = require('gulp-uglify');
-    stylish  = require('jshint-stylish');
-    size     = require('gulp-sizereport');
+ var gulp = require('gulp');
+ var concat = require('gulp-concat');
+ var imagemin = require('gulp-imagemin');
+ var jshint = require('gulp-jshint');
+ var strip = require('gulp-strip-debug');
+ var uglify = require('gulp-uglify');
+ var stylish = require('jshint-stylish');
+ var size = require('gulp-sizereport');
+ var cssmin = require('gulp-cssmin');
 
-// Check the size of the files (imgs, css, js)
+ // Check the size of the files (imgs, css, js)
 
-gulp.task('size', function() {
-    return gulp.src('./dist/*')
-    .pipe(gulp-sizereport);
-    });
+ gulp.task('size', function() {
+   return gulp.src('./prod/dist/**/*')
+     .pipe(size());
+ });
 
-// Configure the JS tasks.
+ // Configure the JS tasks.
 
-gulp.task('js', function() {
-    return gulp.src('src/js/**/*.js')
-    .pipe(gulp-strip-debug())
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(gulp-uglify())
-    .pipe(gulp-concat('scripts.js'))
-    .pipe(gulp.dest('prod/dist/js'));
-    });
+ gulp.task('js', function() {
+   return gulp.src('src/js/**/*.js')
+     // .pipe(strip - debug())
+     .pipe(jshint())
+     .pipe(jshint.reporter('jshint-stylish'))
+     .pipe(uglify())
+     .pipe(concat('scripts.js'))
+     .pipe(gulp.dest('prod/dist/js'));
+ });
 
-// Configure CSS stuff
+ // Configure CSS stuff
 
-gulp.task('css', function() {
-    return gulp.src('src/css/**/*.js')
-    .pipe(gulp-uglify())
-    .pipe(gulp.dest('prod/dist/css');
-    });
+ gulp.task('css', function() {
+   return gulp.src('src/css/**/*.css')
+     .pipe(cssmin())
+     .pipe(gulp.dest('prod/dist/css'));
+});
 
 // Configure image tasks.
 
 gulp.task('images', function() {
-    return gulp.src('src/img/**/*.jpg')
-    .pipe(gulp-imagemin())
-    .pipe(gulp.dest('prod/dist/img');
-    });
+  return gulp.src('src/img/**/*.jpg')
+    .pipe(imagemin())
+    .pipe(gulp.dest('prod/dist/img'));
+});
 
 // Configure which files to watch and what tasks to use on file changes.
 // The watch task can be added later before the build task officially runs.
@@ -54,4 +55,3 @@ gulp.task('images', function() {
 // Define the default task and add the watch task to it.
 
 gulp.task('default', ['js', 'css', 'images', 'size']);
-
